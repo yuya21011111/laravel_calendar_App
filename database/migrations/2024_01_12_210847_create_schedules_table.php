ae0fe49c226e89dc42ff57acb276c07482d9a793
YuyaYamazi <kaimomo12345@gmail.com>
2024-01-13 06:22:52 +0900

    Schedule

---
+++ database/migrations/2024_01_12_210847_create_schedules_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->comment('ユーザーID');
+            $table->string('title')->comment('タイトル');
+            $table->string('description')->nullable()->comment('概要');
+            $table->date('start_date')->comment('開始日');
+            $table->date('end_date')->comment('終了日');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('schedules');
+    }
+};
