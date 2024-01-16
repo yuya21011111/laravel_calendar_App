90067bf04f061be4ee20ea562ff1d3be869a0261
YuyaYamazi <kaimomo12345@gmail.com>
2024-01-16 21:28:58 +0900

    修正

--- resources/views/front/schedule.blade.php
+++ resources/views/front/schedule.blade.php
@@ -4,27 +4,27 @@
 
         <!-- Name -->
         <div>
-            <x-input-label for="title" :value="__('Title')" />
+            <x-input-label for="title" :value="__('タイトル')" />
             <x-text-input id="title" class="block mt-1 w-full" type="text" name="title" :value="old('title')" required autofocus autocomplete="title" />
             <x-input-error :messages="$errors->get('title')" class="mt-2" />
         </div>
         <!-- Description -->
         <div>
-            <x-input-label for="description" :value="__('Description')" />
+            <x-input-label for="description" :value="__('概要')" />
             <textarea id="description" class="block mt-1 w-full" type="text" name="description"   autofocus autocomplete="description">{{ old('description') }}</textarea>
             <x-input-error :messages="$errors->get('description')" class="mt-2" />
         </div>
 
           <!-- Start_date -->
           <div>
-            <x-input-label for="start_date" :value="__('Start_date')" />
+            <x-input-label for="start_date" :value="__('開始時間')" />
             <x-text-input id="start_date" class="block mt-1 w-full" type="date" name="start_date" :value="old('start_date')" required autofocus autocomplete="start_date" />
             <x-input-error :messages="$errors->get('start_date')" class="mt-2" />
         </div>
 
           <!-- End_date -->
           <div>
-            <x-input-label for="end_date" :value="__('End_date')" />
+            <x-input-label for="end_date" :value="__('終了時間')" />
             <x-text-input id="end_date" class="block mt-1 w-full" type="date" name="end_date" :value="old('end_date')" required autofocus autocomplete="end_date" />
             <x-input-error :messages="$errors->get('end_date')" class="mt-2" />
         </div>
